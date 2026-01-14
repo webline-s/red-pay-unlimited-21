34c2eff24842edeeca826254c722d35c7880cc61
webline-s <usmankatun20@gmail.com>
2026-01-14 16:20:41 +0100

    Update WhatsApp and Telegram links

--- src/pages/Community.tsx
+++ src/pages/Community.tsx
@@ -7,11 +7,11 @@ import { MessageCircle, Send } from "lucide-react";
 
 const Community = () => {
   const handleJoinWhatsApp = () => {
-    window.open("https://chat.whatsapp.com/I5j03L8Yy78D7fHEbntFKU", "_blank");
+    window.open("https://chat.whatsapp.com/I5j03L8Yy78D7fHEbntF", "_blank");
   };
 
   const handleJoinTelegram = () => {
-    window.open("https://t.me/Skypay261", "_blank");
+    window.open("https://t.me/Skypay2htu", "_blank");
   };
 
   return (
